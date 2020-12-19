f35d455fcc412e9862b0fda86c505ad61e128dda
Kevin Hsu <kevin.hsukaihao@gmail.com>
2020-12-18 20:55:45 -0800

    New post WIP

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, jsonify, redirect, session, url_for
 from flask_pymongo import PyMongo
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, create_refresh_token, get_jwt_identity, jwt_refresh_token_required
@@ -15,7 +14,7 @@ app.config['SECRET_KEY'] = os.environ.get("GOODPODS_SECRET_KEY")
 jwt = JWTManager(app)
 
 # CORS
-CORS(app, resources={r"*": {"origins": "*", "supports_credentials": True}})
+CORS(app, resources={r"/*": {"origins": "*", "supports_credentials": True}})
 
 # MongoDB
 connectionString = "mongodb+srv://bruno:" + str(os.environ.get("GOODPODS_MONGO_PASSWORD")) + "@cluster0.g2mr3.mongodb.net/goodpods?retryWrites=true&w=majority"
@@ -23,62 +22,78 @@ app.config["MONGO_URI"] = connectionString
 mongo = PyMongo(app)
 db = mongo.db
 
-@app.route("/signup", methods=("POST",))
+@app.route("/signup", methods=["POST"])
 def signup(): 
-    username = request.get("username")
-    password = request.get("password")
+    username = request.json("username")
+    password = request.json("password")
 
     same_username_users = db.users.find( {"username": username } ) 
     if any(True for _ in same_username_users):
-        return jsonify({
-            "status": "error", 
-            "messsage": "username taken"
-            })
+        return jsonify({ "status": "error", "messsage": "username taken" }), 409
 
     pw_hash = bcrypt.hashpw(password, bycrypt.gensalt())
     mongo_id = db.users.insert_one({ "username": username, "password_hash": pw_hash }).inserted_id
 
     access_token = create_access_token(identity=username, expires_delta=False)
-    return jsonify({"status": "success", 'access_token': access_token, 'mongo_id': mongo_id })
+    return jsonify({"status": "success", 'access_token': access_token, 'mongo_id': mongo_id }), 200
 
-@app.route("/login", methods=("POST",))
+@app.route("/login", methods=["POST"])
 def login(): 
-    username = request.get("username")
-    password = request.get("password")
+    username = request.json("username")
+    password = request.json("password")
 
     mongo_user = mongo.db.users.find_one({"username": username})
     pw_hash = mongo_user["password"]
 
     if bcrypt.checkpw(password, hashed):
         access_token = create_access_token(identity=username, expires_delta=False)
-        return jsonify({"status": "success", 'access_token': access_token, 'mongo_id': mongo_user["_id"] })
+        return jsonify({"status": "success", 'access_token': access_token, 'mongo_id': mongo_user["_id"] }), 200
     else: 
-        return jsonify({"status": "error", "message": "incorrect username or password"})
+        return jsonify({"status": "error", "message": "incorrect username or password"}), 401
 
-@app.route("/createPost", methods=("POST",))
+@app.route("/saveToLibrary", methods=['POST'])
+@jwt_required
+def saveToLibrary(): 
+    mongo_id = request.json("mongo_id")
+    # generate uuid
+    # save file
+    # generate transcript
+    # generate soundwave
+    ret = {
+            "clip_id": clip_id, 
+            "mongo_id": mongo_id, 
+            "transcript": transcript, 
+            "waveform": waveform
+            }
+
+    db.clips.insert_one(ret)
+
+
+@app.route("/createPost", methods=["POST"])
 @jwt_required
 def createPost(): 
-    title = request.get("title")
-    caption = request.get("caption")
-    audioFile = request.get("audioFile")
-    mongo_id = request.get("mongo_id")
+    mongo_id = request.json("mongo_id")
+    title = request.json("title")
+    caption = request.json("caption")
+    audioFile = request.json("audioFile")
+    clip_id = request.json("clip_id")
 
     mongo_user = mongo.db.users.find_one({"_id": ObjectId(mongo_id)})
     username = mongo_user["username"]
 
-    # generate transcript
-    # generate soundwave
+    mongo_clip = mongo.db.clips.find_one({"clip_id": clip_id})
+    transcript = mongo_clip["transcript"]
+    waveform = mongo_clip["waveform"]
 
-    newPost = {
-            "user_id": mongo_id, 
+    ret = {
+            "mongo_id": mongo_id, 
+            "username": username, 
             "title": title, 
             "caption": caption, 
-            "audioFile": audioFile, 
-            "transcript": transcript, 
-            "soundwave": soundwave
+            "clip_id": clip_id
             }
-    postId = db.posts.insert_one(newPost).inserted_id
-    return jsonify({"status": "success", 'post_id': postId })
+    postId = db.posts.insert_one(ret).inserted_id
+    return jsonify({"status": "success", 'post_id': postId }), 200
 
 
 
